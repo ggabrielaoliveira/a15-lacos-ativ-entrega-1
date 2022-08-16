9c0a5967023657bf45e5f82323bc08c5c2cf6458
ggabrielaoliveira <gabi.oliveiraa@yahoo.com>
2022-08-16 19:28:00 -0300

    alterações no exercicio

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,7 +1,7 @@
 let coxinhas=prompt("Deseja comer 1 coxinha? S ou N")
 let conta=0
-while(coxinhas==="S"){
+while(coxinhas!=="N"){
     coxinhas=prompt("deseja comer mais 1 coxinha?S ou N")
     conta=conta+2.50
-    console.log(`total da sua conta:\n${conta}`)
 }
+console.log(`total da sua conta:\n${conta}`)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,5 +1,5 @@
 const alfabeto=["a","b","c","d","e"]
 
 for(i=0;i<alfabeto.length;i++){
-    console.log(alfabeto[i])
+    console.log(`${i+1}-${alfabeto[i]}`)
 }
